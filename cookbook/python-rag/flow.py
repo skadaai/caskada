d237df56d5e1342ca8a62e9a289cab8998069c33
zvictor <zvictor@users.noreply.github.com>
2025-05-26 22:45:45 +0200

    improve readability of the flow definition

--- cookbook/python-rag/flow.py
+++ cookbook/python-rag/flow.py
@@ -2,16 +2,22 @@ from brainyflow import Flow
 from nodes import ProcessChunkDocumentsNode, EmbedDocumentsNode, CreateIndexNode, EmbedQueryNode, ReduceChunksAndEmbeddingsNode, RetrieveDocumentNode, GenerateAnswerNode
 from mapreduce import mapreduce
 
-def get_offline_flow():
+def get_document_flow():
     # Create offline flow for document indexing
     process_chunk_docs_node = ProcessChunkDocumentsNode()
     embed_docs_node = EmbedDocumentsNode()
+    
+    process_chunk_docs_node >> embed_docs_node
+    
+    return Flow(start=process_chunk_docs_node)
+
+def get_offline_flow():
+    # Create offline flow for document indexing
+    doc_flow = get_document_flow()
     reduce_chunks_and_embeddings_node = ReduceChunksAndEmbeddingsNode()
     create_index_node = CreateIndexNode()
 
-    mapreduce_flow = mapreduce(Flow(start=process_chunk_docs_node), {"input_key": "texts"})
-    
-    process_chunk_docs_node >> embed_docs_node
+    mapreduce_flow = mapreduce(doc_flow, {"input_key": "texts"})
     mapreduce_flow >> reduce_chunks_and_embeddings_node >> create_index_node
     
     offline_flow = Flow(start=mapreduce_flow)
