77852ff8b6f426189a9a1347f6500cb14096c8ef
zvictor <zvictor@users.noreply.github.com>
2025-06-02 11:21:31 +0200

    start brainyflow-extras

---
+++ python/extras/setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from setuptools import setup
+
+# Read the README.md for the long description
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='brainyflow-extras',
+    version='0.0.1',
+    py_modules=['extras'],
+    author="Victor Duarte",
+    description="Minimalist AI framework in 300 Lines. Enable LLMs to Program Themselves.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://flow.brainy.sh",
+    license='MPL-2.0',
+    classifiers=[
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+    ],
+)
