20d14ee527cac2836264239093b46ebf91c3ea43
zvictor <zvictor@users.noreply.github.com>
2025-06-15 23:22:49 +0200

    update environment installation

--- shell.nix
+++ shell.nix
@@ -1,39 +1,20 @@
+
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  venvDir = "./.venv";
-in pkgs.mkShell {
+pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
+    # System dependencies
     ncurses
     openssh
     git
-    corepack_22
-    nodejs_23
+
+    # Tools for Node environment management
+    corepack_latest
+    nodejs_24
+
+    # Tools for Python environment management
+    python3
     uv
   ];
-
-  packages = [
-    (pkgs.python3.withPackages (python-pkgs: [
-      python-pkgs.pytest
-      python-pkgs.pytest-asyncio
-    ]))
-  ];
-
-  shellHook = ''
-    if [ ! -d "${venvDir}" ]; then
-      echo "Installing python dependencies"
-
-      printf "Creating virtual environment...\n"
-      uv venv --python 3.13 ${venvDir}
-      source "${venvDir}/bin/activate"
-
-      if [ -f "requirements.txt" ]; then
-        printf "Installing dependencies...\n"
-        uv pip install -r requirements.txt
-      fi
-    fi
-
-    source "${venvDir}/bin/activate"
-    export PYTHONPATH="$PWD/python:$PYTHONPATH"
-  '';
 }
+
