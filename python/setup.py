2f716d3fe35ff7f442459cdab317c61d660c3cea
zvictor <181076+zvictor@users.noreply.github.com>
2025-09-07 14:43:14 +0000

    chore: update PY version and publish

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='caskada',
-    version='2.1.0',
+    version='2.2.0',
     py_modules=['caskada'],
     author="Victor Duarte",
     description="Minimalist AI framework in 300 Lines. Enable LLMs to Program Themselves.",
